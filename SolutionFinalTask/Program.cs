9339d0823b2b2c8b85d9c66ca46fc2a75b922d91
Andrew_A <chen_60@mail.ru>
2022-11-30 05:30:09 +0600

    Created and writed SolutionFinalTaskSolution

---
+++ SolutionFinalTask/Program.cs
@@ -0,0 +1,54 @@
+﻿string[] strToChange = { "hello", "2", "world", ":-)", "13" };
+
+string[] CreateArrWithNededElemLgth(string[] array, int lgth)  //НА базе метода пузырьковой сортировки эл. массива по возрастанию
+{
+    int count = 0;
+
+    for (int k = 0; k < array.Length - 1 - k; k++)
+    {
+        for (int i = 0; i < array.Length - 1; i++)
+        {
+            if (array[i].Length > lgth)
+            {
+                //count += 1;
+                string temp = array[i];
+                array[i] = array[i + 1];
+                array[i + 1] = temp;
+            }
+
+        }
+    }
+
+    for (int i = 0; i < array.Length; i++)
+        if (array[i].Length <= lgth)
+        {
+            count += 1;
+        }
+    Console.WriteLine();
+
+    string[] arrNew = new string[count + 1];
+    arrNew[count] = Convert.ToString(count);
+
+    for (int i = 0; i < count; i++)
+    {
+        arrNew[i] = array[i];
+    }
+    array = arrNew;
+
+    return array;
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+
+    }
+    Console.WriteLine();
+}
+
+
+PrintArray(strToChange);
+PrintArray(CreateArrWithNededElemLgth(strToChange, 3));
+Console.WriteLine();
